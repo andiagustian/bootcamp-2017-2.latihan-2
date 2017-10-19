5f9ae766e92be2fba908b533839d9d44581de234
hadimaster <hadimaster65555@gmail.com>
2017-10-19 13:56:19 +0700

    Membuat model Pelanggan

---
+++ src/main/java/com/tabeldata/pln/model/Pelanggan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tabeldata.pln.model;
+
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="pelanggan")
+public class Pelanggan {
+    @Id
+    @GeneratedValue
+    private Integer Id;
+    private Integer namaPelanggan;
+    private BigDecimal saldo;
+    private BigDecimal token;
+    
+}
