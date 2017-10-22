59d8e3627596526fc4c2453ab7b5cf60495a6c2d
hadimaster <hadimaster65555@gmail.com>
2017-10-22 16:17:59 +0700

    Menambahkan kelas Transaksi.java

---
+++ src/main/java/com/tabeldata/pln/model/Transaksi.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tabeldata.pln.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="transaksi")
+public class Transaksi {
+    
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Integer id_pelanggan;
+    private Integer id_token;
+    private Long saldo;
+}
