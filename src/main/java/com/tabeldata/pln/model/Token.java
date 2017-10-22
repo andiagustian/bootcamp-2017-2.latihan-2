a8b9348a9d56b86cbdaa1315670c75e7b8376ad9
hadimaster <hadimaster65555@gmail.com>
2017-10-22 16:10:27 +0700

    Menghapus kelas TokenListrik.java dan diganti dengan kelas Token.java

---
+++ src/main/java/com/tabeldata/pln/model/Token.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tabeldata.pln.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="token_listrik")
+public class Token {
+    
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Integer kode;
+    private Long nama;
+    private Long harga;
+}
