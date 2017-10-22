d01c0e86a9e001fe010d7e5da7386d6f11d98b4e
hadimaster <hadimaster65555@gmail.com>
2017-10-22 16:12:39 +0700

    Menambahkan kelas Role.java

---
+++ src/main/java/com/tabeldata/pln/model/Role.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tabeldata.pln.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="s_role")
+public class Role {
+    
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String nama;
+    @OneToMany(mappedBy = "role")
+    private List<UserRole> listRole = new ArrayList<>();
+}
