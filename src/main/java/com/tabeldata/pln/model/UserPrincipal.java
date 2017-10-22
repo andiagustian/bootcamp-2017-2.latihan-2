7d9ec77190e6da7fa3d7f36d9c42375a9a3cbf7a
hadimaster <hadimaster65555@gmail.com>
2017-10-22 16:28:27 +0700

    Membuat kelas UserPrincipal.java

---
+++ src/main/java/com/tabeldata/pln/model/UserPrincipal.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tabeldata.pln.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ *
+ * @author USER
+ */
+public class UserPrincipal implements UserDetails {
+
+    private User user; 
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> listRole = new ArrayList<>();
+        this.user.getRoles().forEach((ur)->{
+            listRole.add(new SimpleGrantedAuthority(ur.getRole().getNama()));
+        });
+        return listRole;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isActive();
+    }
+
+}
